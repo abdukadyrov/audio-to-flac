19d9aa608c78c765a6d92e1623674c6ec79c1f77
Дмитрий Абдукадыров <abdukadyrov@rocketcrm.bz>
2017-09-11 19:41:57 +0300

    checkpoint

--- src/FFmpegAudio.php
+++ src/FFmpegAudio.php
@@ -18,16 +18,26 @@ class FFmpegAudio
         $this->pathToFFmpeg = $pathToFFmpeg;
     }
 
+    public function __call($name, $arguments)
+    {
+        $methodName = 'set'.ucfirst($name);
+
+        if (method_exists($this, $methodName) && count($arguments) > 0) {
+            $this->$methodName($arguments[0]);
+        }
+
+        return $this;
+    }
+
     /**
      * Set input file
      *
      * @param string $path - path to input file
      * @return FFmpegAudio
      */
-    public function setInput(string $path) :FFmpegAudio
+    private function setInputFile(string $path)
     {
         $this->settings['i'] = $path;
-        return $this;
     }
 
     /**
@@ -36,10 +46,14 @@ class FFmpegAudio
      * @param string $path - path to output file
      * @return FFmpegAudio
      */
-    public function setOutput(string $path) :FFmpegAudio
+    private function setOutputFile(string $path)
     {
         $this->output = $path;
-        return $this;
+    }
+
+    private function setInputSampleRate(int $val = 0)
+    {
+        $this->settings['isr'] = $val;
     }
 
     /**
@@ -48,10 +62,14 @@ class FFmpegAudio
      * @param int $val
      * @return FFmpegAudio
      */
-    public function sampleRate(int $val = 0) :FFmpegAudio
+    private function setOutputSampleRate(int $val = 0)
     {
         $this->settings['osr'] = $val;
-        return $this;
+    }
+
+    private function setInputChannelCount(int $val = 0)
+    {
+        $this->settings['ich'] = $val;
     }
 
     /**
@@ -60,10 +78,107 @@ class FFmpegAudio
      * @param int $val
      * @return FFmpegAudio
      */
-    public function channel(int $val = 0) :FFmpegAudio
+    private function setOutputChannelCount(int $val = 0)
     {
         $this->settings['och'] = $val;
-        return $this;
+    }
+
+    private function setInputSampleFormat($val)
+    {
+        $this->settings['isf'] = $val;
+    }
+
+    private function setOutputSampleFormat($val)
+    {
+        $this->settings['osf'] = $val;
+    }
+
+    private function setUsedChannelCount(int $val = 0)
+    {
+        $this->settings['uch'] = $val;
+    }
+
+    private function setInternalSampleFormat($val)
+    {
+        $this->settings['tsf'] = $val;
+    }
+
+    private function setInputChannelLayout(string $val)
+    {
+        $this->settings['icl'] = $val;
+    }
+
+    private function setOutputChannelLayout(string $val)
+    {
+        $this->settings['ocl'] = $val;
+    }
+
+    private function setCenterMixLevel(int $val = 0)
+    {
+        if ($val < -32 || $val > 32) {
+            return;
+        }
+
+        $this->settings['clev'] = $val;
+    }
+
+    private function setSurroundMixLevel(int $val = 0)
+    {
+        if ($val < -32 || $val > 32) {
+            return;
+        }
+
+        $this->settings['slev'] = $val;
+    }
+
+    private function setLfeMixLevel(int $val = 0)
+    {
+        if ($val < -32 || $val > 32) {
+            return;
+        }
+
+        $this->settings['lfe_mix_level'] = $val;
+    }
+
+    private function setRematrixVolume($val)
+    {
+        $this->settings['rmvol'] = $val;
+    }
+
+    private function setRematrixMaxval($val)
+    {
+        $this->settings['rematrix_maxval'] = $val;
+    }
+
+    private function setSwrFlags(int $val = 0)
+    {
+        $this->settings['flags'] = $val;
+    }
+
+    private function setDitherScale(int $val = 0)
+    {
+        $this->settings['dither_scale'] = $val;
+    }
+
+    private function setDitherMethod(int $val = 0)
+    {
+        $this->settings['dither_method'] = $val;
+    }
+
+    /** end here */
+
+    private function setResampler(string $val = 'swr')
+    {
+        switch ($val) {
+            case 'swr':
+        }
+
+        $this->settings['resampler'] = $val;
+    }
+
+    private function setFilterSize(int $val = 32)
+    {
+        $this->settings['filter_size'] = $val;
     }
 
     /**
